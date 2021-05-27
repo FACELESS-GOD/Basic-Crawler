720346b87570ba11819a4eaada7b6c9027ffe5a9
Mohit Ganorkar <mohotgag@gmail.com>
2021-05-27 22:50:38 +0530

    The networking functionality is added and noew we can get html data from server and save it in desired file

--- Crawler.java
+++ Crawler.java
@@ -2,6 +2,8 @@ import java.util.*;
 import java.io.File;  // Import the File class
 import java.io.IOException;
 import java.io.FileWriter;
+import java.io.*;
+import java.net.*;
 class Crawler
 {
     public static void file(String url)
@@ -27,12 +29,12 @@ class Crawler
             e.printStackTrace();
         }
     }
-    public static void write(String url)
+    public static void write(String url,String line)
     {
         try
         {
              FileWriter myWriter = new FileWriter(url);
-             myWriter.write(url);
+             myWriter.write(line);
              myWriter.close();
              System.out.println("Successfully wrote to the file.");
         }
@@ -44,12 +46,36 @@ class Crawler
 
     }
 
-    public static void crawl(String url)
+    public static void crawl(String url,String ul)
     {
         Stack<Integer> s = new Stack<Integer>();
         Queue<Integer> q = new LinkedList<Integer>();
-        file(url);
-        write(url);
+        file(ul);
+        String line ; 
+        URL u ;//= new URL(url);
+        InputStream is=null;// = u.openStream();
+         BufferedReader br ;// = new BufferedReader(new InputStreamReader(is));
+         try {
+        u = new URL(url);
+        is = u.openStream();  // throws an IOException
+        br = new BufferedReader(new InputStreamReader(is));
+line = br.readLine();/*
+        while ((line = br.readLine();) != null) {
+            System.out.println(line);
+        }*/
+         write(ul,line);
+    } catch (MalformedURLException mue) {
+         mue.printStackTrace();
+    } catch (IOException ioe) {
+         ioe.printStackTrace();
+    } finally {
+        try {
+            if (is != null) is.close();
+        } catch (IOException ioe) {
+            // nothing to see here
+        }
+    }
+   
     }
 
     public static void main(String [] args)
@@ -57,7 +83,9 @@ class Crawler
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the url below :- \n");
         String url = sc.next();
+        System.out.println("Enter the filen name : ");
+         String ul = sc.next();
         System.out.println("the  urls crawled using the given url is as follows : ");
-        crawl(url);
+        crawl(url,ul);
     }
 }
